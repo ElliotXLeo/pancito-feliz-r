38d0315722901966036d87bc078c5aa5567d3760
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-24 20:42:13 -0500

    component CardCupcake

---
+++ src/components/card/CardCupcake.js
@@ -0,0 +1,22 @@
+const CardCupcake = ({cupcake}) => {
+  const { id, sabor, color, descripcion, precio, imagen } = cupcake;
+  return (
+    <article className="cupcakes__cupcake">
+      <figure className="cupcakes__cupcake-image-container">
+        <img src={imagen} alt={descripcion} className="cupcakes__cupcake-image" />
+      </figure>
+      <div className="cupcakes__cupcake-data">
+        <h2 className="cupcakes__cupcake-data-sabor">Sabor: {sabor}</h2>
+        <h3 className="cupcakes__cupcake-data-precio">Precio: {precio}</h3>
+        <h4 className="cupcakes__cupcake-data-color">Color: {color}</h4>
+        <p className="cupcakes__cupcake-data-about">{id}: {descripcion}</p>
+      </div>
+      <div className="cupcakes__cupcake-button-container">
+        <a className="cupcakes__cupcake-button cupcakes__cupcake-button--active">Ver</a>
+        <a className="cupcakes__cupcake-button">Comprar</a>
+      </div>
+    </article>
+  );
+}
+
+export default CardCupcake;
--- src/components/card/CardCupcakes.js
+++ src/components/card/CardCupcakes.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
+import CardCupcake from './CardCupcake';
 
-const CardCupcake = () => {
+const CardCupcakes = () => {
 
   const endpoint = `${process.env.REACT_APP_URL_API}/cupcakes`;
 
@@ -40,23 +41,12 @@ const CardCupcake = () => {
             (
               <div className="cupcakes__cupcakes">
                 {
-                  cupcakes.map(({ id, sabor, color, descripcion, precio, imagen }) => {
+                  cupcakes.map((cupcake) => {
                     return (
-                      <article className="cupcakes__cupcake" key={id}>
-                        <figure className="cupcakes__cupcake-image-container">
-                          <img src={imagen} alt={descripcion} className="cupcakes__cupcake-image" />
-                        </figure>
-                        <div className="cupcakes__cupcake-data">
-                          <h2 className="cupcakes__cupcake-data-sabor">Sabor: {sabor}</h2>
-                          <h3 className="cupcakes__cupcake-data-precio">Precio: {precio}</h3>
-                          <h4 className="cupcakes__cupcake-data-color">Color: {color}</h4>
-                          <p className="cupcakes__cupcake-data-about">{descripcion}</p>
-                        </div>
-                        <div className="cupcakes__cupcake-button-container">
-                          <a className="cupcakes__cupcake-button cupcakes__cupcake-button--active">Ver</a>
-                          <a className="cupcakes__cupcake-button">Comprar</a>
-                        </div>
-                      </article>
+                      <CardCupcake
+                        key={cupcake.id}
+                        cupcake={cupcake}
+                      />
                     );
                   })
                 }
@@ -68,4 +58,4 @@ const CardCupcake = () => {
   );
 }
 
-export default CardCupcake;
+export default CardCupcakes;
