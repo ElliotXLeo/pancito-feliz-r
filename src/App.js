87670ea3b1951e722736dbe6ef296010c843c23e
ElliotXLeo <elliotgaramendi@gmail.com>
2022-03-26 15:54:53 -0500

    component Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import Header from './components/Header';
-import Home from './pages/Home';
-import './App.css';
-
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
 import Cupcakes from './pages/Cupcakes';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
+
+  const credits = {
+    author: 'Elliot Garamendi',
+    currentYear: new Date().getFullYear()
+  };
+
   return (
     <Router>
       <Header />
@@ -20,6 +26,9 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
+      <Footer
+        credits={credits}
+      />
     </Router>
   );
 }
