5d8d11f755b2b80b7f376842ce86a99fd63fbd20
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-04 11:01:37 -0500

    Update use de hooks Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,44 +2,49 @@ import {
   NavLink
 } from 'react-router-dom';
 import '../styles/css/Header.css';
+import { useRef, useEffect } from 'react';
 import logo from '../assets/img/logo.png';
 
-const documentReady = () => {
-  const header = document.querySelector('.header');
-  const headerNav = document.querySelector('.header-nav');
+const Header = () => {
 
-  const headerNavLinkList = document.querySelector('.header-nav__menu-link-list');
-  const headerNavMenuIconContainer = document.getElementById('headerNavMenuIconContainer');
-  const headerNavMenuCloseIconContainer = document.getElementById('headerNavMenuCloseIconContainer');
-  const headerNavLinks = [...document.querySelectorAll('.header-nav__menu-link')];
+  const documentReady = () => {
+    const header = document.querySelector('.header');
+    const headerNav = document.querySelector('.header-nav');
 
-  const documentScroll = () => {
-    header.classList.toggle('header--scroll', window.scrollY > 0);
-    headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
+    const headerNavMenuIconContainer = document.getElementById('headerNavMenuIconContainer');
+    const headerNavMenuCloseIconContainer = document.getElementById('headerNavMenuCloseIconContainer');
+    const headerNavMenuLinkList = document.querySelector('.header-nav__menu-link-list');
+    const headerNavLinks = [...document.querySelectorAll('.header-nav__menu-link')];
+
+    const documentScroll = () => {
+      header.classList.toggle('header--scroll', window.scrollY > 0);
+      headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
+    };
+
+    const openMenu = () => {
+      headerNavMenuLinkList.classList.add('header-nav__menu-link-list--open');
+    };
+
+    const closeMenu = () => {
+      headerNavMenuLinkList.classList.remove('header-nav__menu-link-list--open');
+    };
+
+    document.addEventListener('scroll', documentScroll);
+    headerNavMenuIconContainer.addEventListener('click', openMenu);
+    headerNavMenuCloseIconContainer.addEventListener('click', closeMenu);
+    headerNavLinks.forEach((element) => {
+      element.addEventListener('click', closeMenu);
+    });
   };
+  document.addEventListener('DOMContentLoaded', documentReady);
 
-  const openMenu = () => {
-    headerNavLinkList.classList.add('header-nav__menu-link-list--open');
-  };
-
-  const closeMenu = () => {
-    headerNavLinkList.classList.remove('header-nav__menu-link-list--open');
-  };
-
-  document.addEventListener('scroll', documentScroll);
-  headerNavMenuIconContainer.addEventListener('click', openMenu);
-  headerNavMenuCloseIconContainer.addEventListener('click', closeMenu);
-  headerNavLinks.forEach((element) => {
-    element.addEventListener('click', closeMenu);
-  });
-
-  const headerNavThemeIconContainer = document.getElementById('headerNavThemeIconContainer');
+  const headerNavThemeIconContainer = useRef();
 
   const toggleTheme = () => {
     const body = document.querySelector('.body');
 
     body.classList.toggle('body--light');
-    headerNavThemeIconContainer.classList.toggle('header-nav__theme-icon-container--active');
+    headerNavThemeIconContainer.current.classList.toggle('header-nav__theme-icon-container--active');
 
     if (body.classList.contains('body--light')) {
       localStorage.setItem('darkMode', 'false');
@@ -48,19 +53,16 @@ const documentReady = () => {
     }
   };
 
-  if (localStorage.getItem('darkMode') === 'true') {
-    document.querySelector('.body').classList.remove('body--light');
-    headerNavThemeIconContainer.classList.remove('header-nav__theme-icon-container--active');
-  } else {
-    document.querySelector('.body').classList.add('body--light');
-    headerNavThemeIconContainer.classList.add('header-nav__theme-icon-container--active');
-  }
+  useEffect(() => {
+    if (localStorage.getItem('darkMode') === 'true') {
+      document.querySelector('.body').classList.remove('body--light');
+      headerNavThemeIconContainer.current.classList.remove('header-nav__theme-icon-container--active');
+    } else {
+      document.querySelector('.body').classList.add('body--light');
+      headerNavThemeIconContainer.current.classList.add('header-nav__theme-icon-container--active');
+    }
+  }, []);
 
-  headerNavThemeIconContainer.addEventListener('click', toggleTheme);
-};
-document.addEventListener('DOMContentLoaded', documentReady);
-
-const Header = () => {
   return (
     <header className="header animate__animated animate__fadeIn">
       <nav className="header-nav">
@@ -69,7 +71,11 @@ const Header = () => {
             <img src={logo} alt="Logo Web" className="header-nav__logo" />
           </NavLink>
           <div className="header-nav__theme-menu-container">
-            <button className="header-nav__theme-icon-container" id="headerNavThemeIconContainer">
+            <button
+              className="header-nav__theme-icon-container"
+              ref={headerNavThemeIconContainer}
+              onClick={toggleTheme}
+            >
               <span className="header-nav__theme-icon">🌚</span>
               <span className="header-nav__theme-icon">🌞</span>
             </button>
@@ -88,8 +94,7 @@ const Header = () => {
               <li className="header-nav__menu-link-item">
                 <a href="https://www.linkedin.com/in/elliotgaramendi/" className="header-nav__menu-link header-nav__menu-link--active" target="_blank" rel="noreferrer">Elliot</a>
               </li>
-              <li className="header-nav__menu-link-item header-nav__menu-close-icon-container"
-                id="headerNavMenuCloseIconContainer">
+              <li className="header-nav__menu-link-item header-nav__menu-close-icon-container" id="headerNavMenuCloseIconContainer">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" className="header-nav__menu-close-icon" >
                   <path
                     d="M9.172 16.242 12 13.414l2.828 2.828 1.414-1.414L13.414 12l2.828-2.828-1.414-1.414L12 10.586 9.172 7.758 7.758 9.172 10.586 12l-2.828 2.828z">
