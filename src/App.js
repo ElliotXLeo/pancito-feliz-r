0003bbc2fdc88d646988fb63f702a15f1d977ca5
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-17 15:59:37 -0500

    page Error404

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Cupcakes from './pages/Cupcakes';
 import Footer from './components/Footer';
 import './App.css';
+import Error404 from './pages/Error404';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="/cupcakes" element={<Cupcakes />} />
           <Route path="/nosotros" element={<section className="section animate__animated animate__fadeIn">Nosotros</section>} />
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </main>
       <Footer
